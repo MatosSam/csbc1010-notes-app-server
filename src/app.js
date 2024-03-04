31513bdd5643b7a91f4e84959fbb31537e4eb23a
Matos <desenvolvedor.matos@gmail.com>
2024-03-04 18:19:41 -0500

    Seu mensagem de commit

--- src/app.js
+++ src/app.js
@@ -1,61 +1,45 @@
-const config = require('dotenv').config();
-const cors = require('cors');
-const express = require('express');
-const { MongoClient } = require('mongodb');
+const config = require('dotenv').config()
+const cors = require('cors')
+const express = require('express')
 
-const healthRouter = require('./routes/health');
-const notesRouter = require('./routes/notes');
-const noteRouter = require('./routes/note');
+const healthRouter = require("./routes/health")
+const notesRouter = require("./routes/notes")
+const noteRouter = require("./routes/note")
 
 if (config.error) {
-  throw config.error;
+  throw config.error
 }
 
-const port = process.env.PORT;
-global.port = port;
+const port = process.env.PORT // || 3001
+global.port = port
 
-const corsOptions = {
-  origin: '*',
-  credentials: true,
-  optionSuccessStatus: 200,
-};
+const corsOptions ={
+  origin:'*', 
+  credentials:true,
+  optionSuccessStatus:200,
+}
 
-const app = express();
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const app = express()
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-// TODO-1: Set up database connection.
-// Database connection is already set up here.
-const url = 'mongodb://localhost:27017/';
-MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((client) => {
-    const db = client.db('mydb');
-    console.log('Database connected!');
-    app.locals.db = db;
-    
-    // TODO-2: Upon database connection success, create the relevant table(s) if it does not exist.
-    // Creating relevant database collections if they do not exist
-    db.createCollection('notes')
-      .then(() => {
-        console.log('Notes collection created');
-      })
-      .catch((error) => {
-        console.error('Error creating notes collection:', error);
-      });
-  })
-  .catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
-  });
+/*
+  TODO-1: Settup Database connection
+*/
+
+/*
+  TODO-2: Upon database connection success, create the relavent table(s) if it does not exist.
+*/
 
 app.get('/', (req, res) => {
-  res.send('CSBC1010 Assignment 3 - My Notes');
-});
+  res.send('CSBC1010 Assignment 3 - My Notes')
+})
 
-app.use('/health', healthRouter);
-app.use('/notes', notesRouter);
-app.use('/note', noteRouter);
+app.use("/health", healthRouter)
+app.use("/notes", notesRouter)
+app.use("/note", noteRouter)
 
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+  console.log(`Server listening at http://localhost:${port}`)
+})
--- src/routes/note.js
+++ src/routes/note.js
@@ -1,98 +1,165 @@
-const express = require('express');
-const router = express.Router();
-const { validateNote } = require('../utils/validators');
-const { ObjectId } = require('mongodb');
-
-//Function to generate a unique ID
-function generateUniqueId() {
-  return new ObjectId().toString();
-}
+const express = require('express')
+const router = express.Router()
+const { validateNote } = require('../utils/validators')
 
 /* ------------------------ TODO-4 - Create New Note ------------------------ */
-router.post('/', async (req, res) => {
-  console.log(`[POST] http://localhost:${global.port}/note - Storing a new note`);
+router.post('/', (req, res) => {
+  console.log(`[POST] http://localhost:${global.port}/note - Storing a new note`)
 
-  try {
-    const db = req.app.locals.db;
+  /*
+  	TODO-4:
+  		Given node content
+  		Create a new node and store the node to the database,
+  		Return the newly created note object
 
-    const { text } = req.body;
+  		Note content is stored in variable newText
 
-    const newNote = {
-      id: generateUniqueId(),
-      text,
-      dateCreated: new Date().toISOString(),
-      lastModified: new Date().toISOString()
-    };
+  		Your return object should be something similar to this:
+      	{ id, text, dateCreated, lastModified }
+  */
+  const newText = req.body.text
 
-    const notesCollection = db.collection('notes');
-    await notesCollection.insertOne(newNote);
+  /*
 
-    res.status(201).json(newNote);
-  } catch (error) {
-    console.error('Error creating new note:', error);
-    res.status(500).send('Fail to insert');
+    Your code here...
+
+    const newNote = {} // this is the response object, make sure to replace with actual value
+
+
+
+    // Upon succ, run the following lines to validate the response object and respond to client
+
+    // --- begin of succ flow ---
+    if (!validateNote(newNote)) {
+      res.status(500).send('Invalid data type')
+    }
+	  res.status(201).send({ newNote })
+    // --- end of succ flow ---
+
+
+
+    // Upon fail, run the following lines to respond with an error
+
+    // --- begin of fail flow ---
+    res.status(500).send('Fail to insert')
+    // --- end of fail flow ---
+    
+  */
+
+
+
+  // TODO-4.1: Remove this section once you start working on TODO-4
+  // --- Remove section begins ---
+  const newNote = { id: 2, text: newText, dateCreated: new Date().toISOString().split('T')[0], lastModified: new Date().toISOString().split('T')[0] }
+  if (!validateNote(newNote)) {
+    res.status(500).send('Invalid data type')
   }
-});
+  res.status(201).send({ newNote })
+  // --- Remove section ends ---
+})
+/* -------------------------------------------------------------------------- */
 
 /* ------------------------- TODO-5 - Update A Note ------------------------- */
-router.put('/', async (req, res) => {
-  console.log(`[PUT] http://localhost:${global.port}/note - Updating note`);
+router.put('/', (req, res) => {
+  console.log(`[PUT] http://localhost:${global.port}/note - Updating note`)
 
-  const noteId = req.body.id;
-  const newText = req.body.text;
+  /*
+		TODO-5:
+			Given note id and content
+			Update the note's content with the given id in the database
+			Return the updated note object
 
-  try {
-    const db = req.app.locals.db;
-    const notesCollection = db.collection('notes');
+			Note id is stored in variable noteId
+			Note content is stored in variable newText
 
-    // Update the note in the database
-    const result = await notesCollection.updateOne({ id: noteId }, { $set: { text: newText, lastModified: new Date().toISOString() } });
+			Your return object should be something similar to this:
+        { id, text, dateCreated, lastModified }
+	*/
+	const noteId = req.body.id
+	const newText = req.body.text
 
-    // If the note was updated successfully, return the updated note
-    if (result.modifiedCount === 1) {
-      const updatedNote = await notesCollection.findOne({ id: noteId });
+	/* 
 
-      // Check if the updated note is valid
-      if (!validateNote(updatedNote)) {
-        return res.status(500).send('Invalid data type');
-      }
+		// You code here...
 
-      // Return the updated note
-      return res.send({ updatedNote });
+		const updatedNote = {} // this is the response object, make sure to replace with actual value
+
+
+
+    // Upon succ, run the following lines to validate the response object and respond to client
+
+    // --- begin of succ flow ---
+    if (!validateNote(updatedNote)) {
+      res.status(500).send('Invalid data type')
     }
+	  res.send({ updatedNote })
+    // --- end of succ flow ---
 
-    // If the note has not been updated, return an error message
-    throw new Error('Fail to update');
-  } catch (error) {
-    console.error('Error updating note:', error);
-    return res.status(500).send('Fail to update');
-  }
-});
 
+
+    // Upon fail, run the following lines to respond with an error
+
+    // --- begin of fail flow ---
+    res.status(500).send('Fail to update')
+    // --- end of fail flow ---
+
+	*/
+
+
+
+		// TODO-5.1: Remove this section once you start working on TODO-5
+  	// --- Remove section begins ---
+  	const updatedNote = { id: noteId, text: newText, dateCreated: '2021-04-15', lastModified: new Date().toISOString().split('T')[0]}
+		if (!validateNote(updatedNote)) {
+      res.status(500).send('Invalid data type')
+    }
+  	res.send({ updatedNote })
+  	// --- Remove section ends ---
+})
+/* -------------------------------------------------------------------------- */
 
 /* ------------------------- TODO-6 - Delete A Note ------------------------- */
-router.delete('/', async (req, res) => {
-  console.log(`[DELETE] http://localhost:${global.port}/note - Deleting note`);
+router.delete('/', (req, res) => {
+  console.log(`[DELETE] http://localhost:${global.port}/note - Deleting note`)
 
-  try {
-    const db = req.app.locals.db;
-    const noteId = req.body.id;
-    const notesCollection = db.collection('notes');
+  /*
+	  TODO-6:
+      Given a note id
+		  Delete note with the given id from the database
 
-    // Use the deleteOne method to delete the note with the given ID
-    const result = await notesCollection.deleteOne({ id: noteId });
+		  Note id is stored in variable noteId 
+	*/
+	const noteId = req.body.id
 
-    if (result.deletedCount === 1) {
-      res.send();
-    } else {
-      throw new Error('Fail to delete');
-    }
-  } catch (error) {
-    console.error('Error deleting note:', error);
-    res.status(500).send('Fail to delete');
-  }
-});
+  /*
+
+    // Your code here...
 
 
 
-module.exports = router;
+    // Upon succ, run the following lines to validate the response object and respond to client
+
+    // --- begin of succ flow ---
+    res.send()
+    // --- end of succ flow ---
+
+
+
+    // Upon fail, run the following lines to respond with an error
+
+    // --- begin of fail flow ---
+    res.status(500).send('Fail to delete')
+    // --- end of fail flow ---
+
+  */
+
+
+
+  // TODO-6.1: Remove this section once you start working on TODO-6
+  // --- Remove section begins ---
+  res.send()
+  // --- Remove section ends ---
+})
+/* -------------------------------------------------------------------------- */
+
+module.exports = router
